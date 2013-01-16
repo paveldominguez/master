22699d8578c1a9ec41120c1ada78991c7e32ecf0
Will Davidow <will@unisonagency.com>
2013-01-15 21:29:25 -0500

    Latest
    Removed unnecessary files;

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,50 @@
- // debounce utility from underscorejs.org
- var debounce = function(func, wait, immediate) {
-    var timeout;
-    return function() {
-        var context = this, args = arguments;
-        var later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        if (immediate && !timeout) func.apply(context, args);
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
+var debounce = (function(){
+    var timer = 0;
+    return function(callback, ms){
+        clearTimeout (timer);
+        timer = setTimeout(callback, ms);
     };
-};
+})();
+
 
 jQuery(document).ready(function() {
     // var doc = jQuery(window);
     // jQuery(window).on('resize', function() { // this is for debugging purposes, can be removed when no longer needed
     //     debounce(function() {
     //         console.log('Document width: %dpx', doc.width());
-    //     }, 200, true);
+    //     }, 200);
     // });
 
 
-    var simpleTemplate = [
-        'AA...',
-        'AA...',
-        '.....'];
+    var simpleTemplate = ['AA...','AA...','.....'];
 
-    // tiles test
+    // tiles.js test
     var el = document.getElementById('product-grid'),
     grid = new Tiles.Grid(el);
-
-    // set the new template and resize the grid
     grid.template = Tiles.Template.fromJSON(simpleTemplate);
-    grid.isDirty = true;
-    grid.resize();
-    grid.redraw(true);
 
-    // template is selected by user, not generated so just
-    // return the number of columns in the current template
     grid.resizeColumns = function() {
         return this.template.numCols;
     };
 
-    jQuery('.products-atf .product').each(function(){
-        var self = this;
-        grid.createTile = function() {
-            var tile = new Tiles.Tile(self);
-            return tile;
-        };
-    });
-    
-    var debouncedResize = debounce(function() {
+    // jQuery('#product-grid .product').each(function(i){
+    //     var self = this;
+    //     grid.createTile = function() {
+    //         var tile = new Tiles.Tile(self);
+    //         return tile;
+    //     };
+    // });
+
+    // set the new template and resize the grid
+    grid.isDirty = true;
+    grid.resize();
+
+
+    jQuery(window).on('resize',function() {
         grid.resize();
         grid.redraw(true);
-    }, 200);
-
-    jQuery(window).on('resize',debouncedResize);
+    });
+    
 });
+
 
