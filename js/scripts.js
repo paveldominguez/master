e45dc3067a22ffd92216a83dfd8aea0eedcd0d59
Will Davidow <wdavidow@sapient.com>
2013-01-22 23:51:27 -0500

    Nightly Commit
    - Updates to core JS structure
    
    - Updates to icons
    
    - Updates to category listing styles
    
    - Removed two column layout and layouts folder (not necessary).

--- js/scripts.js
+++ js/scripts.js
@@ -1,80 +1,99 @@
-// var debounce = (function(){
-//     var timer = 0;
-//     return function(callback, ms){
-//         clearTimeout (timer);
-//         timer = setTimeout(callback, ms);
-//     };
-// })();
+var $jQ = jQuery.noConflict(), // Prevent any future library conflicts
+VZ = {}, // Container for firing common and page-specic JavaScript
+COMMON = {}, // NEED TO COME UP WITH BETTER NAME FOR THIS NAMESPACE
+UTIL = {}; // Container for stuff..
 
-// Debounce function from underscore.js
+VZ = {
+// container for commonly used configs throughout site
+    config : {
+
+    },
+    common : {
+        init : function() {
+            // REMOVE THIS CODE LATER...
+            var doc = $jQ(window);
+            $jQ(window).on('resize', function() { // this is for debugging purposes, can be removed when no longer needed
+                debounce(function() {
+                    console.log('Document width: %dpx', doc.width());
+                }, 200);
+            });
+            // END REMOVE THIS CODE LATER...
+        },
+        finalize: function() {
+
+        }
+    },
+    'product-category-list' : {
+        init : function() {
+            COMMON.productHover();
+        }
+    }
+};
+
+COMMON = {
+    productHover : function() {
+        $jQ('#product-grid .product').on({
+            mouseenter : function() {
+                $jQ(this).addClass('over');
+                $jQ(this).find('.product-hover').fadeIn('fast');
+                $jQ(this).find('.rating img').attr('src','img/stars.png'); // temporary..
+            },
+            mouseleave : function() {
+                $jQ(this).removeClass('over');
+                $jQ(this).find('.product-hover').fadeOut('fast');
+                $jQ(this).find('.rating img').attr('src','img/stars-gray.png'); // temporary..
+            },
+            click : function(e) {
+                console.log(e.target.innerHTML);
+            }
+        });
+    }
+};
+
+/*
+ * Debounce (Credit: underscore.js)
+ *
+ */
 var debounce = (function() {
-  var timeout;
-  return function(func, wait, immediate) {
-    var context = this, args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
+    var timeout;
+    return function(func, wait, immediate) {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
     };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
 })();
 
-jQuery(document).ready(function() {
-    var doc = jQuery(window);
-    jQuery(window).on('resize', function() { // this is for debugging purposes, can be removed when no longer needed
-        debounce(function() {
-            console.log('Document width: %dpx', doc.width());
-        }, 200);
+/*
+ *
+ * Markup-based DOM-Ready Execution
+ * Credit: Paul Irish - http://paulirish.com/2009/markup-based-unobtrusive-comprehensive-dom-ready-execution/
+ *
+ */
+
+UTIL = {
+    fire : function(func,funcname, args){
+    var namespace = VZ;  // indicate your obj literal namespace here
+    funcname = (funcname === undefined) ? 'init' : funcname;
+    if (func !== '' && namespace[func] && typeof namespace[func][funcname] == 'function'){
+        namespace[func][funcname](args);
+    }
+},
+loadEvents : function(){
+    var bodyId = document.body.id;
+    UTIL.fire('common');
+    $jQ.each(document.body.className.split(/\s+/),function(i,classnm){
+        UTIL.fire(classnm);
+        UTIL.fire(classnm,bodyId);
     });
+    UTIL.fire('common','finalize');
+}
+};
 
-    jQuery('#product-grid .product').each(function() {
-        var context = this;
-        jQuery(context).hover(
-            function() { // hover in
-                jQuery(context).addClass('over');
-                jQuery(context).find('.product-hover').fadeIn('fast');
-                jQuery(context).find('.rating img').attr('src','img/stars.png');
-            },
-            function() { // hover out
-                jQuery(context).removeClass('over');
-                jQuery(context).find('.product-hover').fadeOut('fast');
-                jQuery(context).find('.rating img').attr('src','img/stars-gray.png');
-            }
-        );
-    });
-
-    // var simpleTemplate = ['AA...','AA...','.....'];
-
-    // // tiles.js test
-    // var el = document.getElementById('product-grid'),
-    // grid = new Tiles.Grid(el);
-    // grid.template = Tiles.Template.fromJSON(simpleTemplate);
-
-    // grid.resizeColumns = function() {
-    //     return this.template.numCols;
-    // };
-
-    // // jQuery('#product-grid .product').each(function(i){
-    // //     var self = this;
-    // //     grid.createTile = function() {
-    // //         var tile = new Tiles.Tile(self);
-    // //         return tile;
-    // //     };
-    // // });
-
-    // // set the new template and resize the grid
-    // grid.isDirty = true;
-    // grid.resize();
-
-
-    // jQuery(window).on('resize',function() {
-    //     grid.resize();
-    //     grid.redraw(true);
-    // });
-    
-});
-
-
+// Initialize
+$jQ(document).ready(UTIL.loadEvents);
