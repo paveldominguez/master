80681023cc6eae3c601f10f0c1eb3b789e1b6184
Will Davidow <wdavidow@sapient.com>
2013-02-20 07:09:40 -0500

    Changed type="button
    Changed type="button" to type="submit"

--- js/content-grid.js
+++ js/content-grid.js
@@ -1,8 +1,8 @@
 var contentGrid = (function() {
+    var $contentGrid = $jQ('#main-column .content-grid');
     var pub = {
         init : function() {
-            var $contentGrid = $jQ('#main-column .content-grid'),
-            $contentItems = $contentGrid.find('.content-item'),
+            var $contentItems = $contentGrid.find('.content-item'),
             $quickviewLinks = $contentItems.find('.quick-view');
             if(!isTouch) {
                 $contentItems.hover(pub.productTileEnter,pub.productTileLeave);
@@ -17,36 +17,39 @@ var contentGrid = (function() {
         productTileLeave : function() {
             $jQ(this).removeClass('over');
             $jQ(this).find('.content-hover').fadeOut('fast');
-            $jQ(this).find('.content-detail').animate( { opacity:1 }, 'fast');
+            $jQ(this).find('.content-detail').animate( { opacity: 1 }, 'fast');
         },
         quickViewHandler : function() {
-            var $quickView = $jQ('.quick-view-overlay'),
-            $contentTile = $jQ(this).parent().parent(),
+            var $quickView = $jQ('#quick-view-overlay'),
+            $parentTile = $jQ(this).parent().parent(),
+            $contentTile = $parentTile.hasClass('featured') ? $parentTile.next() : $parentTile,
             $cTposition = $contentTile.position();
-            console.log('content tile height: %d', $contentTile.outerHeight());
-            $quickView.css(
-                {
-                    'display' : 'block',
-                    'position' : 'absolute',
-                    'top' : $cTposition.top,
-                    'right' : 0,
-                    'height' : ( $contentTile.outerHeight() * 2 ) - 1,
-                    'background-color' : '#FFF',
-                    'z-index' : '99999'
-                }
-            );
-            console.log('window height: %d\nscrolltop of clicked product: %d',$jQ(window).height(),$cTposition.top);
+            console.log($parentTile.next());
+            $quickView.css({
+                'display' : 'block',
+                'top' : $cTposition.top + ($parentTile.hasClass('featured') ? $parentTile.outerHeight() : 0),
+                'height' : ( $contentTile.outerHeight() * 2 ) - 1,
+                'width' : ( $contentGrid.outerWidth() )
+            });
             $jQ('html, body').animate( {
-                scrollTop : $cTposition.top + ($contentTile.outerHeight() / 2 )
+                scrollTop : $cTposition.top + ($contentTile.outerHeight() / 2 ) + ( $parentTile.hasClass('featured') ? $parentTile.outerHeight() : 0 )
             }, 'slow', function() {
+                $jQ('#quick-view-slider').flexslider({
+                    animation: 'slide',
+                    controlNav: "thumbnails",
+                    directionNav: false,
+                    slideshow: false
+                });
+                $jQ('#quick-view-slider .flex-control-thumbs').css('margin-left','-' + ($jQ('#quick-view-slider .flex-control-thumbs').outerWidth() / 2) + 'px');
                 // pseudo:
                 // (on complete of showing quickview block...)
                 // on either:
                 //      scroll (target outside quick view), close quick view
                 //      click (target outside quick view), close quick view
+                //      resize (without debounce), close quick view
                 // in both (previous) cases, remove even listener for scroll/click...
 
-                
+
 
 
                 // $jQ(document).one(
