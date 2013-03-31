14a2fefec46dba6f0abfcb7c2c70ba7ccb82df08
Chris W. Burke <chris@chriswburke.com>
2013-03-31 17:21:57 -0400

    madlib

--- js/home.js
+++ js/home.js
@@ -15,7 +15,26 @@ MLS.home = {
 		//init vzSliders
 		MLS.ui.vzSlider.init();
 
+		//Madlib
+		MLS.home.madlib.init();
 
+
+	},
+
+	madlib: {
+
+		init:function(){
+			//Generic Typeahead
+			$jQ('#madlib-device').typeahead({
+				name: 'devices',
+				remote: 'js/data/devices.json',
+				limit: 10
+			}).on('typeahead:selected',function(){
+				console.log($jQ(this).val());
+				$jQ(this).stop().animate({
+				width: $jQ(this).val().length*18
+				},100);
+			});
+		}
 	}
-
 };
