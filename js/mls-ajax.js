59c16376ce95137ba9ca933980108ba2ea6e5ee5
Carlos A. Cabrera <ccabrera@sapient.com>
2013-05-16 09:47:41 -0400

    js linting

--- js/mls-ajax.js
+++ js/mls-ajax.js
@@ -2,53 +2,53 @@ MLS.ajax = {
     starded: false,
 
     endpoints: {
-        ADD_TO_MINICART: "/services/add_to_cart.json",
-        UPDATE_MINICART: "/services/add_to_cart.json",
-        REMOVE_FROM_MINICART: "/services/add_to_cart.json",
-        GET_MINICART: "/services/add_to_cart.json",
-        GET_CART: "/services/cart.json",
-        GET_CART_SUMMARY: "/services/checkout_cart.json",
-        ARTICLE: "/services/article.json",
-        HOMEPAGE_PRODUCTS: "/services/homepage.json",
-        SEARCH_DEVICES: "/services/devices.json",
+        ADD_TO_MINICART: '/services/add_to_cart.json',
+        UPDATE_MINICART: '/services/add_to_cart.json',
+        REMOVE_FROM_MINICART: '/services/add_to_cart.json',
+        GET_MINICART: '/services/add_to_cart.json',
+        GET_CART: '/services/cart.json',
+        GET_CART_SUMMARY: '/services/checkout_cart.json',
+        ARTICLE: '/services/article.json',
+        HOMEPAGE_PRODUCTS: '/services/homepage.json',
+        SEARCH_DEVICES: '/services/devices.json',
 
         // Products
-        PRODUCT_LISTING: "/services/product-listing.json",
-        PRODUCT_LOAD_MORE: "/services/product-listing.json",
-        PRODUCT_LOAD_ALL: "/services/product-listing.json",
-        PRODUCT_SORT: "/services/product-listing.json",
+        PRODUCT_LISTING: '/services/product-listing.json',
+        PRODUCT_LOAD_MORE: '/services/product-listing.json',
+        PRODUCT_LOAD_ALL: '/services/product-listing.json',
+        PRODUCT_SORT: '/services/product-listing.json',
 
         // Lifestyle
-        LIFESTYLE_LANDING_SEARCH: "/services/lifestyle-landing.json",
+        LIFESTYLE_LANDING_SEARCH: '/services/lifestyle-landing.json',
 
         // CONTENT
-        CONTENT_FILTER: "/services/content.json",
+        CONTENT_FILTER: '/services/content.json',
 
         // SPECIAL OFFERS
-        SPECIAL_OFFERS: "/services/special_offers.json",
+        SPECIAL_OFFERS: '/services/special_offers.json',
 
         // Product Listing Page
-        CATEGORY_PAGE: "/services/category.json",
+        CATEGORY_PAGE: '/services/category.json',
         // Checkout
-        CHECKOUT_SHIPPING_OPTIONS: "/services/checkout_shipping_options.json",
-        CHECKOUT_SELECT_SHIPPING: "/services/checkout_select_shipping.json",
-        CHECKOUT_STEP_1: "/services/checkout_step_1.json",
-        CHECKOUT_STEP_2: "/services/checkout_step_2.json"
+        CHECKOUT_SHIPPING_OPTIONS: '/services/checkout_shipping_options.json',
+        CHECKOUT_SELECT_SHIPPING: '/services/checkout_select_shipping.json',
+        CHECKOUT_STEP_1: '/services/checkout_step_1.json',
+        CHECKOUT_STEP_2: '/services/checkout_step_2.json'
     },
 
-    init: function() {
+    init: function () {
         if (!this.started)
         {
             var $loadingModal = null;
-            $jQ(document).ajaxStart(function() {
-                if ($loadingModal == null)
+            $jQ(document).ajaxStart(function () {
+                if ($loadingModal === null)
                 {
-                    $loadingModal = MLS.modal.open("<img src='/img/ajax-loader.gif' alt='loading...' />", true);
+                    $loadingModal = MLS.modal.open('<img src="/img/ajax-loader.gif" alt="loading..."" />', true);
                 }
             });
 
-            $jQ(document).ajaxStop(function() {
-                if ($loadingModal != null)
+            $jQ(document).ajaxStop(function () {
+                if ($loadingModal !== null)
                 {
                     MLS.modal.close($loadingModal);
                     $loadingModal = null;
@@ -67,8 +67,8 @@ MLS.ajax = {
             data: data,
             cache : false,
             success : success,
-            error : error || function() {
-                MLS.modal.open("Server is not responding,<br />please refresh and try again.");
+            error : error || function () {
+                MLS.modal.open('Server is not responding,<br />please refresh and try again.');
             },
             dataType: 'json'
         });
