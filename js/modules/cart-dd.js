fa5989208b7ca3b0ccc624d67dc4f4abf730d503
Paveldominguez <paveldominguez@gmail.com>
2013-05-20 14:24:20 -0400

    changed the functionality for the product details dd to match the new requirements

--- js/modules/cart-dd.js
+++ js/modules/cart-dd.js
@@ -1,79 +1,77 @@
 MLS.cart.dd = {
     init: function () {
-        $jQ(".color").unbind("click", MLS.cart.dd.get.sizes).click(MLS.cart.dd.get.sizes);
-        $jQ("#pdp-size-select").unbind("change", MLS.cart.dd.get.sizes).change(MLS.cart.dd.get.sizes);
+        console.log("v.10");
+
+        MLS.cart.dd.triggers.ddColor(); // needs to be initialized before the form trigger otherwise it will not update the form
+        MLS.cart.dd.triggers.form();
+
     },
 
-    get: {
-        // colors: function(){
-        //                 var itemId = $jQ("#pdp-add-to-cart-submit").data("data-cart-add");
-        //                 var itemSize = $jQ("#pdp-size-select").val();
+    triggers: {
+        form: function (){
+            $jQ(".data-product-attributes").change(function(){
 
-        //                 MLS.ajax.sendRequest(
-        //             MLS.ajax.endpoints.GET_CART_DD_COLORS,
-        //             {
-        //                 id : itemId,
-        //                 size : itemSize
-        //             },
-        //             function(response){
-        //                     MLS.cart.dd.populate(response, "#pdp-color-select")
-        //                         MLS.cart.dd.get.carousel();
-        //                 }
-        //         );
-        //     },
+                var formAttr = $jQ(this).serialize();
 
-        sizes: function(e) {
-            var itemId = $jQ("#pdp-add-to-cart-submit").data("cart-add"),
-                itemColor = $jQ(".color.active").data("color"),
-                itemSize = $jQ("#pdp-size-select").val();
-
-            MLS.ajax.sendRequest(
-                MLS.ajax.endpoints.GET_CART_DD_SIZES,
-                {
-                    id : itemId,
-                    color : itemColor,
-                    size: itemSize
-                },
-
-                function(response) {
-                    MLS.cart.dd.populate(response, "#pdp-size-select");
-                    $jQ("#pdp-size-select")[0].selectedIndex = $jQ("#pdp-size-select option[value=" + itemSize + "]").index();
-                    MLS.cart.dd.get.carousel(itemId, itemColor, itemSize);
-                }
-            );
+                MLS.cart.dd.getJson(formAttr);
+                //console.log (formAttr);
+            });
         },
 
-        carousel: function(itemId, itemColor, itemSize) {
-            MLS.ajax.sendRequest(
-                MLS.ajax.endpoints.GET_CART_DD_CAROUSEL,
-                {
-                    id : itemId,
-                    size : itemSize,
-                    color : itemColor
-                },
+        ddColor: function(){
+            // update the custom color dd
+            $jQ(".color a").click(function (){
+                var $dd = $jQ(this).parents('form').find("[name=pdpColorSelect]"),
+                color = MLS.util.getUrlParam("color", $jQ(this).attr("href"));
+                $dd.val(color);
+                $jQ(".data-product-attributes").change(); // needs to triger the form change
 
-                function(response) {
-                    var $slides = $jQ("#focus .slides").remove();
-                    $jQ("#focus").html("").append($slides);
-                    MLS.cart.dd.populate(response, "#focus .slides");
+                console.log(' color');
 
-                    $jQ('#focus').flexslider("pause");
-                    $jQ('#focus').data("flexslider", null).flexslider({
-                        animation: "slide",
-                        controlNav: false,
-                        animationLoop: true,
-                        slideshow: true,
-                        sync: "#thumbs"
-                    });
-                }
-            );
+            })
         }
-
     },
 
-    populate : function (data, container) {
-        var values = data.hasOwnProperty('success') ? data.success.responseHTML : data.error.responseHTML
-        $jQ(container).html(values);
-    }
+    getJson : function(formAttr) {
+            MLS.ajax.sendRequest(
+            MLS.ajax.endpoints.GET_CART_DD_ATTRIBUTES,
+            formAttr,
+            function(response) {
+                MLS.cart.dd.populate.init(response);
+            }
+        );
+    },
 
+    populate : {
+        init: function (data){
+            var values = data.hasOwnProperty('success') ? data.success.responseHTML : data.error.responseHTML
+
+            MLS.cart.dd.populate.ddSizes(data.success.responseHTML.sizes);
+            MLS.cart.dd.populate.carousel(data.success.responseHTML.carousel);
+        },
+
+        ddSizes: function(data){
+            $jQ("#pdp-size-select").html(data);
+        },
+
+        carousel : function (data){
+            var $slides = $jQ("#focus .slides").remove();
+
+            $jQ("#focus").html("").append($slides);
+            $jQ('#focus').flexslider("pause");
+
+            $jQ("#focus .slides").html(data);
+            $jQ('#focus').data("flexslider", null).flexslider({
+                animation: "slide",
+                controlNav: false,
+                animationLoop: true,
+                slideshow: true,
+                sync: "#thumbs"
+            });
+        }
+    }
 }
+
+
+
+
