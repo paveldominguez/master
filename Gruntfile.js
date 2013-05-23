6cb0bec5254ce05e5464c9c7d49f29fb9abb6644
Will Davidow <wdavidow@sapient.com>
2013-05-23 13:59:33 -0400

    VWLS-1003
    VWLS-1003 - copy updates, lifestyle landing page
    
    Also, disabled bless in grunftile
    
    Also, enabled debuInfo in compass config within gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,13 +16,13 @@ module.exports = function (grunt) {
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
     // load local tasks from /tasks/grunt folder
-    grunt.loadNpmTasks('grunt-bless', 'grunt-devtools', 'grunt-contrib-concat');
+    grunt.loadNpmTasks(/*'grunt-bless', */'grunt-devtools', 'grunt-contrib-concat');
 
     grunt.initConfig({
         watch: {
             compass: {
                 files: ['sass/**/*.scss'],
-                tasks: ['compass', 'bless:debug', ]
+                tasks: ['compass', /*'bless:debug',*/ ]
             },
             livereload: {
                 files: [
@@ -134,22 +134,22 @@ module.exports = function (grunt) {
                 'js/{,*/}*.js'
             ]
         },
-        bless: {
-            options: {
-                compress: true,
-                cleanup: true,
-                force: true,
-                imports: true,
-                cacheBuster: true
-            },
-            debug: {
-                files: {
-                    'css/styles.css': [
-                        'css/styles.css'
-                    ]
-                }
-            }
-        },
+        // bless: {
+        //     options: {
+        //         compress: true,
+        //         cleanup: true,
+        //         force: true,
+        //         imports: true,
+        //         cacheBuster: true
+        //     },
+        //     debug: {
+        //         files: {
+        //             'css/styles.css': [
+        //                 'css/styles.css'
+        //             ]
+        //         }
+        //     }
+        // },
         compass: {
             options: {
                 sassDir: 'sass',
@@ -161,7 +161,7 @@ module.exports = function (grunt) {
             },
             debug: {
                 options: {
-                    debugInfo: false
+                    debugInfo: true
                 }
             }
         },
@@ -185,8 +185,8 @@ module.exports = function (grunt) {
         if (target === 'debug') {
             return grunt.task.run([
                 'compass',
-                'concat:dist',
-                'bless:debug'
+                'concat:dist'/*,
+                'bless:debug'*/
             ]);
         }
 
@@ -196,15 +196,15 @@ module.exports = function (grunt) {
         if (target === 'debug' || target === 'qa') {
             return grunt.task.run([
                 'compass',
-                'concat:dist',
-                'bless:debug'
+                'concat:dist'/*,
+                'bless:debug'*/
             ]);
         }
 
         if (target === 'dist') {
             return grunt.task.run([
-                'concat:dist',
-                'bless:debug'
+                'concat:dist'/*,
+                'bless:debug'*/
             ]);
         }
     });
