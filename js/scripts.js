cfb280d70493654cb6477a959d6a042ef85da968
Daniel Cohen <dcohen2@sapient.com>
2013-05-29 15:31:26 -0400

    Reverted checkout success init to use PDP functions

--- js/scripts.js
+++ js/scripts.js
@@ -155,7 +155,7 @@ MLS = {
     'checkout-success-page': {
         init : function () {
             MLS.header.init();
-            MLS.checkoutSuccess.init();
+            MLS.productDetail.init();
             MLS.miniCart.init();
         }
     },
